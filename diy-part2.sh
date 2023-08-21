ad2cadf9e4f5d383b0cf89b8a9f7eaf38be1bdd9
leewooshung <55313458+leewooshung@users.noreply.github.com>
2023-08-22 05:48:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,7 +4,18 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
+#openclash
+
+mkdir package/others
+mkdir package/others/openclash
+cd package/others/openclash
+git init
+git remote add origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+cd ../../
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
